97542a34769f54e9c582dd1c4dbc8eaaa89a72b3
root <root@localhost>
2022-09-20 01:56:38 +0000

    finsihed multi books

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :user_books
   resources :users
-  root 'home#index'
-  resources :books
-  get '/hi/there'
+  root 'user_books#index'
+  
+  resources :books do
+    member do
+      get :delete
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
